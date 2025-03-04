eb65e4491e42a211d3490835b88187afad34a5ed
Kevin Zhu <kzhu2099@gmail.com>
2025-03-03 20:26:16 -0600

    name main

--- examples/standard.py
+++ examples/standard.py
@@ -1,36 +1,37 @@
 from mynyt import MyNYT
 
-news = MyNYT(
-    'your.email@gmail.com',
-    'your appp pass word',
-    rss_links = None,
-    style_sheet = None
-)
+if __name__ == '__main__':
+    news = MyNYT(
+        'your.email@gmail.com',
+        'your appp pass word',
+        rss_links = None,
+        style_sheet = None
+    )
 
-news.get_all_stories(
-    rotate_through_feeds = True
-)
+    news.get_all_stories(
+        rotate_through_feeds = True
+    )
 
-news.remove_duplicates(
-    all_stories = None,
-)
+    news.remove_duplicates(
+        all_stories = None,
+    )
 
-news.trim_to_length(
-    length = 12,
-    stories = None
-)
+    news.trim_to_length(
+        length = 12,
+        stories = None
+    )
 
-news.convert_news_to_html(
-    stories = None,
-    image_story_html_template = None,
-    imageless_story_html_template = None,
-    main_div_styles = None
-)
+    news.convert_news_to_html(
+        stories = None,
+        image_story_html_template = None,
+        imageless_story_html_template = None,
+        main_div_styles = None
+    )
 
-news.send_email(
-    recipient = 'your.email@gmail.com',
-    main_subject = 'Daily NYT',
-    timezone = 'US/Eastern',
-    story_html_body = None,
-    main_html_template = None
-)
+    news.send_email(
+        recipient = 'your.email@gmail.com',
+        main_subject = 'Daily NYT',
+        timezone = 'US/Eastern',
+        story_html_body = None,
+        main_html_template = None
+    )
